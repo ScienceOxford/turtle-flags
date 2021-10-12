4a9e6255efdeb00d921743e392437c2cd9c333a9
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2021-10-12 14:53:33 +0100

    add starter code

---
+++ examples/turtle-starter.py
@@ -0,0 +1,20 @@
+from turtle import *
+bgcolor('whitesmoke')
+
+color('blue', 'pink')
+height = 100
+width = 200
+
+begin_fill()
+
+forward(width)
+left(90)
+forward(height)
+left(90)
+
+forward(width)
+left(90)
+forward(height)
+left(90)
+
+end_fill()
